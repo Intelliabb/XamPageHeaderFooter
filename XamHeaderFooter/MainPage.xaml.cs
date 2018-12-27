4c9f3090805776f0e0242e1e5562844f37647792
Hussain N Abbasi <hnabbasi@outlook.com>
2018-12-26 22:28:21 -0600

    More goodness

---
+++ XamHeaderFooter/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+
+namespace XamHeaderFooter
+{
+    public partial class MainPage : BaseContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            var button = sender as Button;
+            Page page = null;
+
+            switch (button.CommandParameter)
+            {
+                case "0":
+                    page = new HeaderPage();
+                    break;
+                case "1":
+                    page = new FooterPage();
+                    break;
+                case "2":
+                default:
+                    page = new HeaderFooterPage();
+                    break;
+            }
+
+            Navigation.PushAsync(page);
+        }
+    }
+}
